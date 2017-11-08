1cc50af5d0f1ebe12890049288fa2b19026b5509
alexander <ag.saldivar@gmail.com>
2017-11-07 20:12:01 -0600

    add production db

--- src/app.js
+++ src/app.js
@@ -11,11 +11,16 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // db
-mongoose.connect('mongodb://localhost:27017/trimmerwork')
+if (process.env.NODE_ENV = 'development') {
+	mongoose.connect('mongodb://localhost:27017/trimmerwork')
+}
+if (process.env.NODE_ENV = 'producion') {
+	mongoose.connect('mongodb://heroku_86w0zdvn:mgomru9o8qgjd3g8svi9t9iq44@ds149495.mlab.com:49495/heroku_86w0zdvn')
+}
 const db = mongoose.connection
 
 db.on("error", console.error.bind(console, "connection error"))
-db.once("open", function(callback){
+db.once("openUri", function(callback){
   console.log("Connection Succeeded")
 })
 
