148d9c4845d5021687387c6c809e5d9f33db2cca
alexander <ag.saldivar@gmail.com>
2017-11-07 20:56:10 -0600

    trying to connect to prod db

--- src/app.js
+++ src/app.js
@@ -11,12 +11,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 // db
-if (process.env.NODE_ENV = 'development') {
-	mongoose.connect('mongodb://localhost:27017/trimmerwork')
-}
-if (process.env.NODE_ENV = 'production') {
 	mongoose.connect('mongodb://heroku_86w0zdvn:mgomru9o8qgjd3g8svi9t9iq44@ds149495.mlab.com:49495/heroku_86w0zdvn')
-}
+
 const db = mongoose.connection
 
 db.on("error", console.error.bind(console, "connection error"))
