43a11d9e0524ad0aa1fb36741d951f51ee6dbedb
alexander <ag.saldivar@gmail.com>
2017-11-07 23:37:48 -0600

    move cors middleware above routes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const app = express()
 
 const routes = require('./routes')
 
+app.use(cors())
 app.use('/api',routes)
 app.use(bodyParser.json())
-app.use(cors())
 
 // db
 	mongoose.connect('mongodb://heroku_86w0zdvn:mgomru9o8qgjd3g8svi9t9iq44@ds149495.mlab.com:49495/heroku_86w0zdvn')
