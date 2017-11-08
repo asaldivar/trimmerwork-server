5aa935fa1652e83d31f80acad3520321e80c7583
alexander <ag.saldivar@gmail.com>
2017-11-07 19:58:20 -0600

    update Job and Resume models

--- src/models/Job.js
+++ src/models/Job.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose')
 const Schema = require('mongoose').Schema
 
 const JobSchema = new Schema({
-
+	companyName: String,
+	companyWebsite: String,
+	companyEmail: String,
+	jobTitle: String,
+	jobDescription: String,
+	jobLocation: String,
+	jobCompensation: String,
+	jobCategory: String,
+	jobType: String,
+	jobAccommodations: String,
+	jobApplication: String
 })
 
 const Job = mongoose.model('Job', JobSchema)
--- src/models/Resume.js
+++ src/models/Resume.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const ResumeSchema = new Schema({
-
+	name: String,
+	email: String,
+	location: String,
+	jobTitle: String,
+	jobCategory: String,
+	references: Boolean,
+	workExperience: String,
+	coverLetter: String,
+	additionalInfo: String,
+	contactInfo: String
 })
 
 const Resume = mongoose.model('Resume', ResumeSchema)
